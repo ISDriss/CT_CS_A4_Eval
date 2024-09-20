9f0ca914c3ccfdf58bb1b75f6861a5051b7bc538
Ilian Sebti <ilian.sebti@gmail.com>
2024-09-20 14:55:12 +0200

    run.sh venv fix

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 python3 -m venv .venv
-source .venv/Scripts/activate
+#source .venv/Scripts/activate      #only for filthy bash + windows users
+source .venv/bin/activate
 pip install -r requirements.txt
 fastapi dev server.py
